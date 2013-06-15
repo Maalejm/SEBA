1f6089993e222aa0e643d0cfb85bbdacb8627714
Al-waleed Shihadeh <wshihadeh@live.com>
2013-06-15 11:18:01 +0200

    change the project

---
+++ SlifeWebSite/app/controllers/Ads.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.validation.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Ads  extends Application {
+
+	 public static void index() {
+	        List<Ad> ads = Ad.all().fetch();
+	        render(ads);
+	    }
+
+	 public static void list(String search, Integer size, Integer page) {
+	        List<Ad> ads = null;
+	        page = page != null ? page : 1;
+	        if(search.trim().length() == 0) {
+	            ads = Ad.all().fetch(page, size);
+	        } else {
+	            search = search.toLowerCase();
+	            ads = Ad.find("lower(headline) like ?1 OR lower(description) like ?2", "%"+search+"%", "%"+search+"%").fetch(page, size);
+	        }
+	        render(ads, search, size, page);
+	    }
+	 
+	 public static void getImage(long id) {
+  	   //final Ad ad = Ad.findById(id);
+  	   //notFoundIfNull(ad);
+  	   //response.setContentTypeIfNotSet(ad.image.type());
+  	   //renderBinary(ad.image.get());
+  	
+  	  
+  	}
+}
